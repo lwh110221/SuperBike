28b2806616ce017e32b87b3cdf74267e0a2c344a
lwh110221 <2285813721@qq.com>
2024-04-13 15:30:19 +0800

    @LWH110221 增加订单接口和服务

--- src/com/lwhao/dao/OrderDao.java
+++ src/com/lwhao/dao/OrderDao.java
@@ -1,7 +1,18 @@
-package com.lwhao.dao;/**
+package com.lwhao.dao;
+
+import com.lwhao.bean.Order;
+import java.util.List;
+
+/**
  * @author : Luowenhao221
- * @date   : 2024/4/13 12:10
+ * @date : 2024/4/13 12:10
  * @Project : SuperBike
- *
-*/public interface OrderDao {
+ */
+public interface OrderDao {
+    public int saveOrder(Order order);
+    public List<Order> queryAllOrders();
+    //更新订单状态
+    public void updateOrderStatus(int i, String orderId);
+    //查看订单
+    public List<Order> queryMyOrders(int userId);
 }
--- src/com/lwhao/service/OrderService.java
+++ src/com/lwhao/service/OrderService.java
@@ -1,7 +1,45 @@
-package com.lwhao.service;/**
+package com.lwhao.service;
+
+import com.lwhao.bean.*;
+import java.util.List;
+
+/**
  * @author : Luowenhao221
- * @date   : 2024/4/13 12:40
+ * @date : 2024/4/13 12:40
  * @Project : SuperBike
- *
-*/public class OrderService {
+ */
+public interface OrderService {
+    /**
+     * 查询所有订单
+     */
+    public List<Order> queryAllOrders();
+    /**
+     * 创建订单
+     * @param cart
+     * @param userId
+     * @return
+     */
+    public String createOrder(Cart cart, Integer userId);
+    /**
+     *查询我的订单
+        * @param id
+        * @return
+        */
+    public List<Order> queryMyOrders(Integer id);
+    /**
+     *  发货
+     * @param orderId
+     */
+    public void sendOrder(String orderId);
+    /**
+     *  收货
+     * @param orderId
+     */
+    public void receiveOrder(String orderId);
+    /**
+     *  显示订单详情
+     * @param orderId
+     * @return
+     */
+    List<OrderItem> showOrderItem(String orderId);
 }
