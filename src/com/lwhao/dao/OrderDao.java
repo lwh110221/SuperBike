4c3978a02a7e85d50849aaf0a336cfc7a4d335b1
lwh110221 <2285813721@qq.com>
2024-04-13 15:22:22 +0800

    @LWH110221 Add OrderDao interface and OrderService class

---
+++ src/com/lwhao/dao/OrderDao.java
@@ -0,0 +1,7 @@
+package com.lwhao.dao;/**
+ * @author : Luowenhao221
+ * @date   : 2024/4/13 12:10
+ * @Project : SuperBike
+ *
+*/public interface OrderDao {
+}
---
+++ src/com/lwhao/service/OrderService.java
@@ -0,0 +1,7 @@
+package com.lwhao.service;/**
+ * @author : Luowenhao221
+ * @date   : 2024/4/13 12:40
+ * @Project : SuperBike
+ *
+*/public class OrderService {
+}
