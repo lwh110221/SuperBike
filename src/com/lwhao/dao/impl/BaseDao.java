e950e5eb5556ab1b7a759ace5b1232c28ab61f8a
lwh110221 <2285813721@qq.com>
2024-04-15 10:23:59 +0800

    @LWH110221 更新了BaseDao.java文件，增加了基本的数据库增删改查操作

--- src/com/lwhao/dao/impl/BaseDao.java
+++ src/com/lwhao/dao/impl/BaseDao.java
@@ -2,6 +2,9 @@ package com.lwhao.dao.impl;
 
 import com.lwhao.util.JDBCUtils;
 import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.BeanHandler;
+import org.apache.commons.dbutils.handlers.BeanListHandler;
+import org.apache.commons.dbutils.handlers.ScalarHandler;
 
 
 import java.sql.Connection;
@@ -31,6 +34,59 @@ public abstract class BaseDao {
         } catch (SQLException e) {
             e.printStackTrace();
             throw new RuntimeException(e);//抛出运行时异常
+        }
+    }
+
+    /**
+     *查询返回多个Javabean的SQL语句
+     * @param type 返回的对象类型
+     * @param sql 执行的sql语句
+     * @param args sql对应的参数值
+     * @param <T> 返回的类型的泛型
+     * @return
+     */
+    public <T> T queryForOne(Class<T> type,String sql,Object ... args) {
+        Connection connection = JDBCUtils.getConnection();
+        try {
+            return queryRunner.query(connection,sql,new BeanHandler<T>(type),args);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    /**
+     *查询返回多个Javabean的SQL语句
+     * @param type 返回的对象类型
+     * @param sql 执行的sql语句
+     * @param args sql对应的参数值
+     * @param <T> 返回的类型的泛型
+     * @return
+     */
+    public <T> List<T> queryForList(Class<T> type, String sql, Object ... args) {
+        Connection connection = JDBCUtils.getConnection();
+        try {
+            return queryRunner.query(connection, sql, new BeanListHandler<T>(type), args);//BeanListHandler()用于返回多行多列的数据
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * 执行返回一行一列的sql语句
+     * @param sql 执行的sql语句
+     * @param args sql对应的参数值
+     * @return
+     */
+    public Object queryForSingleValue(String sql,Object ... args) {
+        Connection connection = JDBCUtils.getConnection();
+        try {
+            return queryRunner.query(connection, sql, new ScalarHandler(), args);//ScalarHandler()用于返回一行一列的数据
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
         }
     }
 }
