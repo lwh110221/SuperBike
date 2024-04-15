66df7e355d12a5ba8dd6ccc98a6b9d416dd17738
lwh110221 <2285813721@qq.com>
2024-04-15 10:32:07 +0800

    @LWH110221 fix bug：更改用户的增删改查操作"

--- src/com/lwhao/dao/impl/UserDaoImpl.java
+++ src/com/lwhao/dao/impl/UserDaoImpl.java
@@ -16,76 +16,98 @@ import java.util.List;
  * 实现UserDao接口
  */
 public class UserDaoImpl extends BaseDao implements UserDao {
+    /**
+     * 根据用户名查询用户
+     * @param username
+     * @return
+     */
+    @Override
+    public User querybyUsername(String username) {
+        String sql = "select `id`,`username`,`password`,`email`,`address`,`receiver`,`phone` from t_user where username = ?";
+        return queryForOne(User.class,sql,username);
+    }
 
-        /**
-        * 添加用户
-        * @param user
-        * @return
-        */
-        @Override
-        public int addUser(User user) {
-            String sql = "insert into t_user(`username`,`password`,`email`) values(?,?,?)";
-            return update(sql, user.getUsername(), user.getPassword(), user.getEmail());
-        }
+    /**
+     * 根据用户名和密码查询用户
+     * @param username
+     * @param password
+     * @return
+     */
 
-        /**
-        * 更新用户
-        * @param user
-        * @return
-        */
-        @Override
-        public int updateUser(User user) {
-            String sql = "update t_user set `username`=?,`password`=?,`email`=? where id=?";
-            return update(sql, user.getUsername(), user.getPassword(), user.getEmail(), user.getId());
-        }
+    @Override
+    public User querybyUsernameAndPassword(String username, String password) {
+        String sql = "select * from t_user where username = ? and password = ?";
+        return queryForOne(User.class,sql,username,password);
+    }
 
-        @Override
-        public User queryUserById(Integer id) {
-            String sql = "select * from t_user where id = ?";
-            return queryForOne(User.class, sql, id);
-        }
+    /**
+     * 插入保存用户信息
+     * @param user
+     * @return
+     */
+    @Override
+    public int saveUser(User user) {
+        String sql = "insert into t_user(`username`,`password`,`email`,`address`,`receiver`,`phone`) values(?,?,?,?,?,?)";
+        return update(sql,user.getUsername(),user.getPassword(),user.getEmail(),user.getAddress(),user.getReceiver(),user.getPhone());
+    }
 
-        @Override
-        public User querybyUsername(String username) {
-            String sql = "select `id`,`username`,`password`,`email` from t_user where username = ?";
-            return queryForOne(User.class, sql, username);
-        }
+    /**
+     * 添加用户
+     * @param user
+     * @return
+     */
 
-        @Override
-        public User querybyUsernameAndPassword(String username, String password) {
-            String sql = "select * from t_user where username = ? and password = ?";
-            return queryForOne(User.class, sql, username, password);
-        }
+    @Override
+    public int addUser(User user) {
+        String sql = "insert into t_user(`username`,`password`,`email`,`address`,`receiver`,`phone`) values(?,?,?,?,?,?)";
+        return update(sql,user.getUsername(),user.getPassword(),user.getEmail(),user.getAddress(),user.getReceiver(),user.getPhone());
+    }
 
-        @Override
-        public int saveUser(User user) {
-            String sql = "insert into t_user(`username`,`password`,`email`) values(?,?,?)";
-            return update(sql, user.getUsername(), user.getPassword(), user.getEmail());
-        }
+    /**
+     * 根据id删除用户
+     * @param i
+     * @return
+     */
 
-        @Override
-        public int deleteUserById(int i) {
-            String sql = "delete from t_user where id = ?";
-            return update(sql, i);
-        }
+    @Override
+    public int deleteUserById(int i) {
+        String sql = "delete from t_user where id = ?";
+        return update(sql,i);
+    }
 
-        @Override
+    /**
+     * 更新用户信息
+     * @param user
+     * @return
+     */
 
-        public List<User> queryUsers() {
-            String sql = "select * from t_user";
-            return queryForList(User.class, sql);
-        }
+    @Override
+    public int updateUser(User user) {
+        String sql = "update t_user set `username`=?,`password`=?,`email`=?,`address`=?,`receiver`=?,`phone`=? where id=?";
+        return update(sql,user.getUsername(),user.getPassword(),user.getEmail(),user.getAddress(),user.getReceiver(),user.getPhone(),user.getId());
+    }
 
-        @Override
-        public Integer queryForPageTotalCount() {
-            String sql = "select count(*) from t_user";
-            Number count = (Number) queryForSingleValue(sql);
-            return count.intValue();
-        }
+    /**
+     * 根据id查询用户
+     * @param id
+     * @return
+     */
 
-        @Override
-        public List<User> queryForPageItems(int begin, int pageSize) {
-            String sql = "select * from t_user limit ?,?";
-            return queryForList(User.class, sql, begin, pageSize);
-        }
+    @Override
+    public User queryUserById(Integer id) {
+        String sql = "select * from t_user where id=?";
+        return queryForOne(User.class,sql,id);
+    }
+
+    /**
+     * 查询所有用户
+     * @return
+     */
+
+    @Override
+    public List<User> queryUsers() {
+        String sql = "select * from t_user";
+        return queryForList(User.class,sql);
+    }
+
 }
