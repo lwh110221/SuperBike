eb6a4b2e9b3d884c8af47f7db595181bf3c3d910
lwh110221 <2285813721@qq.com>
2024-04-22 21:03:11 +0800

    @LWH110221 增加过滤器，在JDBCUtils类中增加了获取数据库连接池中的连接、提交事务并关闭释放连接、回滚事务并关闭释放连接和关闭连接，放回数据库连接池的方法。在TransactionFilter类中，添加了一个过滤方法，用于在请求处理完毕后提交事务，如果在处理请求过程中发生异常，则回滚事务。"

---
+++ src/com/lwhao/filter/TransactionFilter.java
@@ -0,0 +1,46 @@
+package com.lwhao.filter;
+
+import com.lwhao.util.JDBCUtils;
+
+import java.io.IOException;
+import jakarta.servlet.*;
+
+/**
+ * @author : Luowenhao221
+ * @date : 2024/4/22 20:50
+ * @Project : SuperBike
+ */
+public class TransactionFilter implements Filter {
+    /**
+     * 初始化方法
+     * @param filterConfig
+     */
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    /**
+     * 过滤方法
+     * @param servletRequest
+     * @param servletResponse
+     * @param filterChain
+     * @throws IOException
+     */
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        try {
+            filterChain.doFilter(servletRequest,servletResponse);
+            JDBCUtils.commitAndClose();  //提交事务
+        } catch (Exception e) {
+            JDBCUtils.rollbackAndClose();  //回滚事务
+            e.printStackTrace();
+            throw new RuntimeException(e);  //把异常抛给tomcat服务器统一展示友好提示页面
+        }
+    }
+    /**
+     * 销毁方法
+     */
+    @Override
+    public void destroy() {
+    }
+}
