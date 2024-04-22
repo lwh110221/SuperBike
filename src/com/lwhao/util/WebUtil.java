02733f967f089ac6c416fe8be28f702330fbdabf
lwh110221 <2285813721@qq.com>
2024-04-22 20:06:30 +0800

    @LWH110221 增加WebUtil类，在WebUtil类中增加了快速注入和字符串转整数的方法。

---
+++ src/com/lwhao/util/WebUtil.java
@@ -0,0 +1,43 @@
+package com.lwhao.util;
+
+import org.apache.commons.beanutils.BeanUtils;
+
+import java.util.Map;
+
+/**
+ * @author : Luowenhao221
+ * @date : 2024/4/22 20:02
+ * @Project : SuperBike
+ */
+public class WebUtil {
+    /**
+     * 快速注入
+     * @param value
+     * @param bean
+     * @return
+     */
+    public static Object copyParamToBean(Map value, Object bean) {
+        try {
+            BeanUtils.populate(bean,value);
+        } catch (Exception e) {
+            e.printStackTrace();
+}
+        return bean;
+    }
+    /**
+     * 将字符串转换成整数
+     * @param strInt
+     * @param defaultValue
+     * @return
+     */
+
+    public static int parseInt(String strInt,int defaultValue) {
+        int n = 0;
+        try {
+            n = Integer.parseInt(strInt);
+        } catch (Exception e) {
+            return defaultValue;
+        }
+        return n;
+    }
+}
